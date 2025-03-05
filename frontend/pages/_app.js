3510c5cca558500c47c2e9daaaa3f4b4ea6d64a9
fumi <82708314+fumifumi0831@users.noreply.github.com>
2025-03-05 21:42:36 +0900

    Update _app.js to add AuthProvider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import { ThemeProvider } from '../components/ThemeProvider';
+import { AuthProvider } from '../contexts/AuthContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
